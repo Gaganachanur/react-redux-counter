fba94260b59d77cf005d68ab56f3dc5b6946b598
Gaganachanur <76946122+Gaganachanur@users.noreply.github.com>
2023-01-13 17:15:40 +0530

    2 files modified

--- Counter.js
+++ Counter.js
@@ -1,39 +1,43 @@
 import React from 'react';
-import { connect } from 'react-redux'
+
+import {useSelector,useDispatch} from 'react-redux'
 
 // Action creator
-function incrementCounter(num) {
-  return { type: 'INCREMENT', num: num }
-}
-function decrement(num){
-return {type:'DECREMENT', num:num}
-}
 
-function Counter(props) {
+
+
+function Counter() {
+
+  const hookSelector = useSelector((state)=>{
+    return {
+      count: state.count
+    };
+  })
+
+  const dispatch = useDispatch();
+
   function handleClick() {
-    props.incrementCounter(1);
+    dispatch(incrementCounter(1));
   }
 
   function handleClickDecrement(){
-    props.decrement(1)
+    dispatch(decrement(1))
+  }
+
+  function incrementCounter(num) {
+    return { type: 'INCREMENT', num: num }
+  }
+  function decrement(num){
+  return {type:'DECREMENT', num:num}
   }
 
   
     return <div>
-    <p>{props.count}</p>
+    <p>{hookSelector.count}</p>
     <button onClick={handleClick}>Increment</button>
     <button onClick={handleClickDecrement}>decrement</button>
     </div>;
 }
 
-function mapStateToProps(state) {
-  return {
-    count: state.count
-  };
-}
-const mapDispatchToProps = {
-  incrementCounter,
-  decrement
-}
 
-export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+export default Counter;
